b02d3ec10e1daa9c3a2222f4cbe65024a3fc4a78
javicia <javier.garcia.perez@hotmail.com>
2023-03-23 06:22:35 +0100

    update

---
+++ app/src/main/java/com/javier/passlive/Detail/Detail_record.java
@@ -0,0 +1,16 @@
+package com.javier.passlive.Detail;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.javier.passlive.R;
+
+public class Detail_record extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_record);
+    }
+}
