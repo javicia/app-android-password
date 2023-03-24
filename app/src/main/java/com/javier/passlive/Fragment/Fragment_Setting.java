e6aee66e3761bb9fcb7347e9d3dcbc95671af412
javicia <javier.garcia.perez@hotmail.com>
2023-03-25 00:10:41 +0100

    update

--- app/src/main/java/com/javier/passlive/Fragment/Fragment_Setting.java
+++ app/src/main/java/com/javier/passlive/Fragment/Fragment_Setting.java
@@ -3,6 +3,7 @@ package com.javier.passlive.Fragment;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +16,8 @@ import androidx.fragment.app.Fragment;
 import com.javier.passlive.BBDD.BBDDHelper;
 import com.javier.passlive.MainActivity;
 import com.javier.passlive.R;
+
+import java.io.File;
 
 
 public class Fragment_Setting extends Fragment {
@@ -52,12 +55,12 @@ public class Fragment_Setting extends Fragment {
         Import_File.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getActivity(), "Importar archivo", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getActivity(), "Importar archivo", Toast.LENGTH_SHORT).show();
+                Export_Record();
             }
         });
         return view;
     }
-
     private void Dialog_Delete_All_Record() {
         Button Btn_Yes, Btn_Cancel;
         dialog.setContentView(R.layout.box_dialog_delete_all_record);
@@ -86,5 +89,15 @@ public class Fragment_Setting extends Fragment {
         });
         dialog.show();
         dialog.setCancelable(false);
+    }
+    private void Export_Record() {
+        //Creamos el nombre de la carpeta
+        File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS
+        ), "PassLive");
+        boolean fileFolder = false;
+        //Si la carpeta no exista, la creamos
+        if(!file.exists()){
+        fileFolder = file.mkdirs();
+        }
     }
 }
