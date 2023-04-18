6b5bd43bf45994d2725fce7af51cdfe9dccfb59f
javicia <javier.garcia.perez@hotmail.com>
2023-04-18 06:28:11 +0200

    comentarios

--- app/src/main/java/com/javier/passlive/DAO/BankDAO.java
+++ app/src/main/java/com/javier/passlive/DAO/BankDAO.java
@@ -1,16 +1,9 @@
 package com.javier.passlive.DAO;
 
-import android.annotation.SuppressLint;
-import android.content.ContentValues;
 import android.content.Context;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.javier.passlive.BBDD.BBDD_Helper;
-import com.javier.passlive.BBDD.Constans;
-import com.javier.passlive.Model.Bank;
-
-import java.util.ArrayList;
 
 public class BankDAO {
     private static SQLiteDatabase db;
